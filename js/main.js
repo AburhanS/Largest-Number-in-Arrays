b6f1e9139ff65dc2f7786bcc6b5ecc775d34b079
Burhan <ahmetilov20@gmail.com>
2023-08-03 02:14:50 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/* const subArrays = [
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+];
+
+const largestOfFour = [];
+
+for (let i = 0; i < subArrays.length; i++) {
+    let maxValue = subArrays[i][0];
+
+for (let j = 1; j < subArrays[i].length; j++) {
+    if (subArrays[i][j] > maxValue) {
+    maxValue = subArrays[i][j];
+    }
+}
+
+largestOfFour.push(maxValue);
+}
+
+console.log(largestOfFour); */
